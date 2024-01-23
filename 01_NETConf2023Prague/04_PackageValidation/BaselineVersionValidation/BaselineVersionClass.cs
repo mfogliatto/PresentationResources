770310ccd292947838a5e13b9c74ee9c0f0b6303
Marco Fogliatto <2962955+mfogliatto@users.noreply.github.com>
2024-01-23 12:34:43 +0100

    [NETConf2023Prague] Add PackageValidation example

---
+++ 01_NETConf2023Prague/04_PackageValidation/BaselineVersionValidation/BaselineVersionClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaselineVersionValidation
+{
+    public static class BaselineVersionClass
+    {
+        public static void SomeMethod() // API we have in v1.0
+        //public static void SomeMethod(string someString) // API we are trying to pack in v1.1
+        {
+        }
+    }
+}
---
+++ 01_NETConf2023Prague/04_PackageValidation/FrameworkValidation/FrameworkValidationClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FrameworkValidation
+{
+    public class FrameworkValidationClass
+    {
+#if NET6_0
+        public void DoSomething()
+        {
+            // Do something for .NET 6.0
+        }
+#else
+    public void DoSomething(string input)
+    {
+            // Do something for other when targeting other .NET versions.
+    }
+#endif
+    }
+}
