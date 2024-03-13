8ea9138bd8cb6a2445995863d1e79d92ef46c3b9
Marco Fogliatto <2962955+mfogliatto@users.noreply.github.com>
2024-03-13 11:16:23 +0100

    [NETConf2023Prague] Update FrameworkValidation demo

--- 01_NETConf2023Prague/04_PackageValidation/FrameworkValidation/FrameworkValidationClass.cs
+++ 01_NETConf2023Prague/04_PackageValidation/FrameworkValidation/FrameworkValidationClass.cs
@@ -4,16 +4,16 @@ namespace FrameworkValidation
 {
     public class FrameworkValidationClass
     {
-#if NET6_0
+#if NET6_0_OR_GREATER
         public void DoSomething()
         {
             // Do something for .NET 6.0
         }
 #else
-    public void DoSomething(string input)
-    {
+        public void DoSomething(string input)
+        {
             // Do something for other when targeting other .NET versions.
-    }
+        }
 #endif
     }
 }
